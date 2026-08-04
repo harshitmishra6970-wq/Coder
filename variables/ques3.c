6179a3cd140ce090786ebed5a04eb4488b852961
harshit <harshit.mishra6970@gmail.com>
2026-08-04 13:09:43 +0530

    circle of area

---
+++ variables/ques3.c
@@ -0,0 +1,11 @@
+ #include<stdio.h>
+
+  int main () {
+    float r, area;\
+    printf("enter redius:");
+    scanf("%f", & area);
+
+    area = 3.14 * r * r;
+    printf("area = %.2f", area);
+    return 0;
+  }

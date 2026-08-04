18272e948dc090527ebba677a85adbea5a65171a
harshit <harshit.mishra6970@gmail.com>
2026-08-04 15:20:27 +0530

    rectangle area

---
+++ variables/ques7.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+     float l,b;  // l-length b-breadth
+    printf("enter the two number:");
+    scanf("%f%f", &l,&b);
+    printf("rectangle area = %.2f", l * b);
+
+    return 0;
+}

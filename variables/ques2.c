7dc0145404099b7850645b1fd795db774a41b3b5
harshit <harshit.mishra6970@gmail.com>
2026-08-04 12:42:06 +0530

    area of square

---
+++ variables/ques2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+    float side;
+    printf("enter side");
+    scanf("%d", &side);
+     
+    printf("area is : %f", side * side);
+
+    return 0;
+}
+    
+    

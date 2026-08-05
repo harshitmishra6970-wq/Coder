310474bd20b103cd8dab97acc01967d780c597c2
harshit <harshit.mishra6970@gmail.com>
2026-08-05 17:55:06 +0530

    celsius to ferenheit

---
+++ variables/declaration/celsius_to_fehrenheit.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+   float celsius = 35, fehrenheit;
+   
+
+   fehrenheit = (celsius * 9/5) + 32;
+    printf("fehrenheit = %.2f", fehrenheit);
+
+   return 0;
+
+}

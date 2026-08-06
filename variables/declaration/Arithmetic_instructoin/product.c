267b7ac24ed991376d6367df206bb60003f81921
harshit <harshit.mishra6970@gmail.com>
2026-08-07 00:01:49 +0530

    product.c

---
+++ variables/declaration/Arithmetic_instructoin/product.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+   int main() {
+    int a,b;
+    printf("enter the number a\n");
+    scanf("%d", &a);
+    printf("enter the numer b\n");
+    scanf("%d", &b);
+ printf("product = %d", a * b);
+ return 0;   
+   }

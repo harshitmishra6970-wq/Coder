7d3ea43f4159160c440657024f93af848c0c8bc6
harshit <harshit.mishra6970@gmail.com>
2026-08-06 23:43:44 +0530

    Addition

---
+++ variables/declaration/Arithmetic_instructoin/Addition.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+   int main() {
+    int a,b;
+       printf("enter the number a\n");
+       scanf("%d", &a);
+       printf("enter the number b\n");
+       scanf("%d", &b);
+       
+       printf("sum = %d", a + b);
+
+       return 0;
+
+
+       
+   }

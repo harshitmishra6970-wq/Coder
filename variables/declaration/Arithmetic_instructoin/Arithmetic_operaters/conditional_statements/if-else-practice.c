d3cbc8679bac1bfc031da120e33226f47fc917ba
harshit <harshit.mishra6970@gmail.com>
2026-08-09 22:11:27 +0530

    if-else-practice1

---
+++ variables/declaration/Arithmetic_instructoin/Arithmetic_operaters/conditional_statements/if-else-practice.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+   int main() {
+    int n;
+    printf("enter the number:");
+    scanf("%d", &n);
+
+    if (0<n) {
+      printf("positive\n");
+
+    }
+    else{
+printf("negative\n");
+
+    }
+    printf("thank you");
+
+    return 0;
+   }

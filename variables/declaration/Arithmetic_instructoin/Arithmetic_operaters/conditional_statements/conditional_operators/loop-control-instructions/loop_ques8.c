b34d6a2d3f7ecfffb300232974bd78ac1ab5379c
harshit <harshit.mishra6970@gmail.com>
2026-08-13 12:32:23 +0530

    factorial_loop

---
+++ variables/declaration/Arithmetic_instructoin/Arithmetic_operaters/conditional_statements/conditional_operators/loop-control-instructions/loop_ques8.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+  int main() {
+    int n;
+    int fact = 1;
+
+    printf("enter number:");
+    scanf("%d", &n);
+
+    for(int i = 1; i <= n; i++)
+    {
+        fact = fact * i;
+    }
+     printf("factorial = %d", fact);
+
+     return 0;
+  }

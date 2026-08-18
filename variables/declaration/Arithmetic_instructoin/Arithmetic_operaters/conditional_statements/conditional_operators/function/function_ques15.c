9b0680eca7a8c2b663e5f4cf813a4f5eb0b40071
harshit <harshit.mishra6970@gmail.com>
2026-08-18 18:02:03 +0530

    table

---
+++ variables/declaration/Arithmetic_instructoin/Arithmetic_operaters/conditional_statements/conditional_operators/function/function_ques15.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+ void table(int n) {
+    for (int i = 1; i <= 10; i++)
+    {
+    printf("%d * %d = %d\n", i,n,n * i);
+    }
+ }
+ 
+ int main() {
+    int n;
+    printf("enter the n number:");
+    scanf("%d",&n);
+    table(n);
+    return 0;
+ }
+    
+    
+    
+ 

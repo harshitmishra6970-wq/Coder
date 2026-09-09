9e9156b0d25d2a5c8866af23fdf1b1f8f34e34e9
harshit <harshit.mishra6970@gmail.com>
2026-09-09 10:49:17 +0530

    string using pointer

---
+++ variables/Arithmetic_instructoin/Arithmetic_operaters/conditional_statements/conditional_operators/function/pointers/pointer_to_pointer/Arrays/strings/string-using-pointers/string-usinng_pointer.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void printstring(char arr[]);
+int main() {
+char *canchange = "hello world";
+puts(canchange);
+canchange = "hello";
+puts(canchange);
+return 0;
+}
+void printstring(char arr[])
+{
+    for (int  i = 0; arr[i] != '\0'; i++)
+    {
+        printf("%c", arr[i]);
+    }
+    printf("\n");
+}

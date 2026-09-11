33e53538a397beda016acaa9571303dd5c23785e
harshit <harshit.mishra6970@gmail.com>
2026-09-11 15:12:32 +0530

    array question practice

---
+++ variables/Arithmetic_instructoin/Arithmetic_operaters/conditional_statements/conditional_operators/function/pointers/pointer_to_pointer/Arrays/strings/string-using-pointers/stander-library-function/array-ques.c
@@ -0,0 +1,32 @@
+//question1. Take a string input from the user using %c?
+
+#include <stdio.h>
+#include <string.h>
+void printstring(char arr[]);
+int countlength(char arr[]);
+
+int main() {
+    char str[100];
+    char ch;
+    int i = 0;
+
+    while (ch != '\n')
+    {
+        scanf("%c", &ch);
+        str[i] = ch;
+        i++;
+    }
+    str[i] = '\0';
+    puts(str);
+}
+    int countlength(char  arr[]) {
+        int count = 0;
+        for (int  i = 0; arr[i] != '\0'; i++)
+        {
+            count++;
+        }
+        return count-1;
+        
+    }
+
+    
